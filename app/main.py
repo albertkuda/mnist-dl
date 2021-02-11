b09a0327c896d80f8801fcb2383ee3d6651ca644
albertkuda <78933503+albertkuda@users.noreply.github.com>
2021-02-12 02:01:58 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,12 +4,6 @@ from app.torch_utils import transform_image, get_prediction
 
 app = Flask(__name__)
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-def allowed_file(filename):
-    # xxx.png
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
@@ -23,23 +17,8 @@ def predict():
             img_bytes = file.read()
             tensor = transform_image(img_bytes)
             prediction = get_prediction(tensor)
-            data = {'prediction': prediction.item(), 'class_name': str(prediction.item())}
+            data = {'я думаю, это: ': prediction.item()}
             return jsonify(data)
         except:
             return jsonify({'error': 'error during prediction'})
         
-        
-@app.route('/plot')
-def build_plot():
-
-    img = io.BytesIO()
-
-    y = [1,2,3,4,5]
-    x = [0,2,1,3,4]
-    plt.plot(x,y)
-    plt.savefig(img, format='png')
-    img.seek(0)
-
-    plot_url = base64.b64encode(img.getvalue()).decode()
-
-    return '<img src="data:image/png;base64,{}">'.format(plot_url)
